f87d635c4255138353e51c742d465385204ad50f
kishimotonico <kishimotonico@gmail.com>
2021-01-06 14:36:07 +0900

    Update サーバからの更新データをフロントに反映するように

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import TaskObject from './TaskObject';
 import { LyricalSocket } from './lyricalSocket';
 import config from './config.json';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { setValue } from "./actions";
 
 const App: FC = () => {
+  const dispatch = useDispatch();
+
   // Websocketを用意
   useEffect(() => {
     const socket = LyricalSocket.instance;
     socket.socket.emit("message", "hey");
-    socket.socket.emit("message", "hey");
+
+    socket.socket.on("update", (operation: {taskObjectId: string, afterValue: number}) => {
+      console.log(operation);
+      dispatch(setValue(operation.taskObjectId, operation.afterValue)); // サーバでのバリデーションを信じる
+    });
 
     return () => {
       // console.log('useEffect-return');
