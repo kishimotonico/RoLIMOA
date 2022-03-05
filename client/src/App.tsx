473d754935ea3d497e51350e87eba4d7792bba8b
kishimotonico <kishimotonico@gmail.com>
2022-03-05 09:05:42 +0900

    Prepare for migration Material UI v4 to v5
    移行はできたけど色がいまいちだったので、ひとまず準備のみで保留。

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,24 @@
 import React, { FC, useEffect } from 'react';
-import { Route, Routes } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { useRecoilState } from 'recoil';
+import { Route, Routes } from 'react-router';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { RootState } from './features';
 import { scoreStateSlice } from './features/score';
 import { phaseStateSlice } from './features/phase';
 import { teamsStateSlice } from './features/teams';
+import { connectedDevicesStateSlice } from './features/connectedDevices';
 import { connectionState } from './atoms/connectionState';
 import { HomePage } from './HomePage';
 import { LoadingOverlay } from "./LoadingOverlay";
 import { ScoreInputPage } from './ScoreInputPage';
 import { AdminPage } from './AdminPage';
 import { StreamingOverlayPage } from './StreamingOverlayPage';
-import { LyricalSocket } from './lyricalSocket';
 import { LocalTimerClock } from './LocalTimerClock';
-import { connectedDevicesStateSlice } from './features/connectedDevices';
 import { GetDeviceName } from './SettingModal';
+import { LyricalSocket } from './lyricalSocket';
+
+const theme = createMuiTheme();
 
 const App: FC = () => {
   const [isConnect, setIsConnect] = useRecoilState(connectionState);
@@ -53,15 +56,17 @@ const App: FC = () => {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/score/red" element={<ScoreInputPage fieldSide="red" />} />
-        <Route path="/score/blue" element={<ScoreInputPage fieldSide="blue" />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/streaming_overlay" element={<StreamingOverlayPage />} />
-      </Routes>
-      <LoadingOverlay loading={!isConnect}/>
-      <LocalTimerClock />
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/score/red" element={<ScoreInputPage fieldSide="red" />} />
+          <Route path="/score/blue" element={<ScoreInputPage fieldSide="blue" />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/streaming_overlay" element={<StreamingOverlayPage />} />
+        </Routes>
+        <LoadingOverlay loading={!isConnect}/>
+        <LocalTimerClock />
+      </ThemeProvider>
     </>
   );
 }
