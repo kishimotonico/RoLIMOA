5db12b29ee7d72c1039dae63e161a369c60bb4d7
kishimotonico <kishimotonico@gmail.com>
2022-03-19 11:33:43 +0900

    Add ui/CenterFlexを追加
    ついでにScreenPageに左右入れ替えボタンを復元

--- client/src/pages/ScreenPage.tsx
+++ client/src/pages/ScreenPage.tsx
@@ -1,24 +1,41 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { ScoreBlock } from 'components/ScoreBlock';
 import { TimerDisplay } from 'components/TimerDisplay';
 import { usePlaySoundEffect } from 'functional/usePlaySoundEffect';
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, IconButton } from '@mui/material';
+import { CenterFlex } from 'ui/CenterFlex';
+import CachedIcon from '@mui/icons-material/Cached';
 
 export const ScreenPage: FC = () => {
   usePlaySoundEffect();
 
+  const [reverse, setReverse] = useState(false);
+  const onReverseClick = () => { setReverse(toggle => !toggle) };
+  
   return (
     <Box sx={{ padding: '2em' }}>
       <Grid container spacing={6}>
         {/* スコア */}
-        <Grid item container sx={{ justify: "space-between", alignItems: "center" }}>
+        <Grid item container sx={{ 
+          justify: "space-between", 
+          alignItems: "center", 
+          flexDirection: reverse ? "row-reverse" : "row",
+        }}>
           <Grid item xs={5}>
             <ScoreBlock fieldSide="blue" />
           </Grid>
           <Grid item xs={2}>
-            {/* <IconButton aria-label="delete" color="default" onClick={onReverseClick}>
-              <CachedIcon />
-            </IconButton> */}
+            <CenterFlex sx={{
+              opacity: 0.1,
+              transition: "opacity",
+              "&:hover": {
+                opacity: 1.0,
+              },
+            }}>
+              <IconButton aria-label="delete" color="default" onClick={onReverseClick}>
+                <CachedIcon />
+              </IconButton>
+            </CenterFlex>
           </Grid>
           <Grid item xs={5}>
             <ScoreBlock fieldSide="red" />
---
+++ client/src/ui/CenterFlex.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import { Box, BoxProps } from '@mui/material';
+
+type CenterFlexProp = BoxProps;
+
+export const CenterFlex: FC<CenterFlexProp> = ({
+  sx,
+  children,
+  ...rest
+}) => {
+  return (
+    <Box {...rest} sx={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      ...sx,
+    }}>
+      {children}
+    </Box>
+  );
+};
