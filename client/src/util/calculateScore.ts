18edc4e7f9b01e29cf6325f3f397fb6ef1d6897c
kishimotonico <kishimotonico@gmail.com>
2023-02-26 02:40:08 +0900

    Refactor calculateScore.ts

--- client/src/util/calculateScore.ts
+++ client/src/util/calculateScore.ts
@@ -1,5 +1,5 @@
 import { PhaseState } from "slices/phase";
-import { ScoreStateType } from "../slices/score";
+import { ScoreStateType, TaskStateType } from "slices/score";
 import { evaluateFormula, FormulaExpression, ReferencedStatsType } from "./formulaExpression";
 import * as Phase from "util/PhaseStateUtil";
 
@@ -18,6 +18,11 @@ type ScoreRuleFormulaExpressionType = {
   expression: FormulaExpression,
 };
 
+type ScoreResultType = {
+  value: number,
+  refs?: Record<string, number>,
+};
+
 function isScoreRuleSimpleType(arg: any): arg is ScoreRuleSimpleType {
   return arg.format === "simple";
 }
@@ -26,55 +31,70 @@ function isScoreRuleFormulaExpressionType(arg: any): arg is ScoreRuleFormulaExpr
 }
 
 // `scoreRule`に基づいてスコアを算出する
-export function calculateScore(scoreRule: ScoreRuleType, scoreState: ScoreStateType, phaseState: PhaseState): { value: number, refs?: Record<string, number> } {
-  const taskObjects = scoreState.tasks;
-
+export function calculateScore(scoreRule: ScoreRuleType, scoreState: ScoreStateType, phaseState: PhaseState): ScoreResultType {
   // タスクオブジェクトと係数の組み合わせによるシンプルなルール
   if (isScoreRuleSimpleType(scoreRule)) {
-    const subTotal = scoreRule.expression.map(({coefficient, id}) => {
-      const val = taskObjects[id];
-      if (val === undefined) {
-        console.warn(`ふぇぇ…IDが"${id}"の要素がないよぉ`);
-        return NaN; // error
-      }
-      return val * coefficient;
-    });
-    const value = subTotal.reduce((acc, cur) => acc + cur, 0);
+    const value = calculateScoreSimple(scoreRule, scoreState.tasks);
     return { value };
   }
 
   // 汎用的な計算表現による複雑なルール記述
   if (isScoreRuleFormulaExpressionType(scoreRule)) {
-    let matchElapsedSec = phaseState.elapsedSecond;
+    return calculateScoreFormulaExpression(scoreRule, scoreState, phaseState);
+  }
+  
+  throw new Error("ふぇぇ…点数計算でエラーが発生したよぉ");
+}
+
+function calculateScoreSimple(scoreRule: ScoreRuleSimpleType, taskObjects: TaskStateType): number {
+  const subTotal = scoreRule.expression.map(({coefficient, id}) => {
+    const val = taskObjects[id];
+    if (val === undefined) {
+      console.warn(`ふぇぇ…IDが"${id}"の要素がないよぉ`);
+      return NaN; // error
+    }
+    return val * coefficient;
+  });
+  return subTotal.reduce((acc, cur) => acc + cur, 0);
+}
+
+function calculateScoreFormulaExpression(scoreRule: ScoreRuleFormulaExpressionType, scoreState: ScoreStateType, phaseState?: PhaseState): ScoreResultType {
+  // 点数計算に、適切な経過時間を取得する
+  function matchElapsedSec(scoreState: ScoreStateType, phaseState?: PhaseState): number {
+    if (! phaseState) {
+      return NaN;
+    }
     // Vゴール時にはVゴールタイムを経過時間にする
     if (scoreState.vgoal) {
-      matchElapsedSec = scoreState.vgoal;
-    }
-    // 競技開始前は0秒として扱う
-    if (phaseState.current.id !== "match") { // TODO: ハードコーディング気味なので良いアイデアがあれば改善
-      matchElapsedSec = 0;
+      return scoreState.vgoal;
     }
     // 競技終了後は競技時間を経過時間として扱う
     if (phaseState.current.id === "match_finish") {
-      matchElapsedSec = Phase.getConfig("match").time;
+      return Phase.getConfig("match").time;
     }
-
-    const referencedStats: ReferencedStatsType = {
-      taskObjects,
-      matchStats: {
-        elapsedTime: matchElapsedSec,
-        isVgoaled: scoreState.vgoal !== undefined ? 1 : 0,
-        vgoalTime: scoreState.vgoal ?? NaN,
-      },
-    };
-    const value = evaluateFormula(scoreRule.expression, referencedStats);
-    return {
-      value,
-      refs: referencedStats.refRecords
-    };
+    // 競技開始前は0秒として扱う
+    if (phaseState.current.id !== "match") { // TODO: ハードコーディング気味なので良いアイデアがあれば改善
+      return 0;
+    }
+    // 競技中は、経過時間をそのまま
+    return phaseState.elapsedSecond;
   }
 
+  const taskObjects = scoreState.tasks;
+  const elapsedTime = matchElapsedSec(scoreState, phaseState);
+
+  const referencedStats: ReferencedStatsType = {
+    taskObjects,
+    matchStats: {
+      elapsedTime,
+      isVgoaled: scoreState.vgoal !== undefined ? 1 : 0,
+      vgoalTime: scoreState.vgoal ?? NaN,
+    },
+  };
+
+  const value = evaluateFormula(scoreRule.expression, referencedStats);
   return {
-    value: NaN,
+    value,
+    refs: referencedStats.refRecords
   };
 }
