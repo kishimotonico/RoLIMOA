85c10806af4a32ed57bc472a65240d959a1613a2
kishimotonico <kishimotonico@gmail.com>
2023-08-28 23:36:32 +0900

    📙 （ルール変更）NHK2023 → ダミーロボコン

--- client/src/custom/rule.score.ts
+++ client/src/custom/rule.score.ts
@@ -7,48 +7,16 @@ type ScoreOutputType = {
 
 export function score(stat: CurrentMatchStateType): ScoreOutputType {
   // ここにスコアの計算処理をかく
-  // return NaN;
+  //
+  // config.json の rule.score に `"format": "implement"` を指定した場合のみ、
+  // この実装が有効
+  //
+  // 例えば下記のようなケースでは、TypeScriptでの得点計算を実装するのでなく、
+  // config.json の rule.score に `"format": "simple"` を指定する方が簡単
+  //
+  // ```
+  // return stat.taskObjects["A_1_point"] + stat.taskObjects["B_10_point"] * 10;
+  // ```
 
-  // NHK学ロボ2023のルールの設定
-  // - P01, P02, P03側が赤コート。P09,P10,P11側が青コート
-  // - 青リングの入ったポールは1、赤リングは2で示す
-  if (stat.fieldSide === "red") {
-    const type1
-      = (stat.globalObjects["P01"] === 2 ? 1 : 0)
-      + (stat.globalObjects["P02"] === 2 ? 1 : 0)
-      + (stat.globalObjects["P03"] === 2 ? 1 : 0);
-    const type2
-      = (stat.globalObjects["P04"] === 2 ? 1 : 0)
-      + (stat.globalObjects["P05"] === 2 ? 1 : 0)
-      + (stat.globalObjects["P07"] === 2 ? 1 : 0)
-      + (stat.globalObjects["P08"] === 2 ? 1 : 0);
-    const type3
-      = (stat.globalObjects["P06"] === 2 ? 1 : 0);
-    const oppenentType1
-      = (stat.globalObjects["P09"] === 2 ? 1 : 0)
-      + (stat.globalObjects["P10"] === 2 ? 1 : 0)
-      + (stat.globalObjects["P11"] === 2 ? 1 : 0);
-
-    return type1 * 10 + type2 * 30 + type3 * 70 + oppenentType1 * 25;
-  } 
-  if (stat.fieldSide === "blue") {
-    const type1
-      = (stat.globalObjects["P09"] === 1 ? 1 : 0)
-      + (stat.globalObjects["P10"] === 1 ? 1 : 0)
-      + (stat.globalObjects["P11"] === 1 ? 1 : 0);
-    const type2
-      = (stat.globalObjects["P04"] === 1 ? 1 : 0)
-      + (stat.globalObjects["P05"] === 1 ? 1 : 0)
-      + (stat.globalObjects["P07"] === 1 ? 1 : 0)
-      + (stat.globalObjects["P08"] === 1 ? 1 : 0);
-    const type3
-      = (stat.globalObjects["P06"] === 1 ? 1 : 0);
-    const oppenentType1
-      = (stat.globalObjects["P01"] === 1 ? 1 : 0)
-      + (stat.globalObjects["P02"] === 1 ? 1 : 0)
-      + (stat.globalObjects["P03"] === 1 ? 1 : 0);
-
-    return type1 * 10 + type2 * 30 + type3 * 70 + oppenentType1 * 25;
-  }
   return NaN;
 }
--- client/src/custom/rule.vgoal.ts
+++ client/src/custom/rule.vgoal.ts
@@ -2,33 +2,9 @@ import { CurrentMatchStateType as VgoalConditionInputType } from '@/util/current
 
 export function condition(stat: VgoalConditionInputType): boolean | undefined {
   // ここにVゴール可能の条件をかく
-  // return true;
+  //
+  // config.json の rule.vgoal.condition に `"format": "implement"` の指定が
+  // ある場合のみこの実装が有効
 
-  // NHK学ロボ2023のルールの設定
-  // - P01, P02, P03側が赤コート。P09,P10,P11側が青コート
-  // - 青リングの入ったポールは1、赤リングは2で示す
-  if (stat.fieldSide === "red") {
-    return (
-      stat.globalObjects["P01"] === 2 &&
-      stat.globalObjects["P02"] === 2 &&
-      stat.globalObjects["P03"] === 2 &&
-      stat.globalObjects["P04"] === 2 &&
-      stat.globalObjects["P05"] === 2 &&
-      stat.globalObjects["P06"] === 2 &&
-      stat.globalObjects["P07"] === 2 &&
-      stat.globalObjects["P08"] === 2
-    );
-  } 
-  if (stat.fieldSide === "blue") {
-    return (
-      stat.globalObjects["P04"] === 1 &&
-      stat.globalObjects["P05"] === 1 &&
-      stat.globalObjects["P06"] === 1 &&
-      stat.globalObjects["P07"] === 1 &&
-      stat.globalObjects["P08"] === 1 &&
-      stat.globalObjects["P09"] === 1 &&
-      stat.globalObjects["P10"] === 1 &&
-      stat.globalObjects["P11"] === 1
-    );
-  }
+  return true;
 }
