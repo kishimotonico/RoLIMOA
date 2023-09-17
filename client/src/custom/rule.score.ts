6bb78fc6d0ed77f08e92f5fb2adb5ee8d0be220f
kishimotonico <kishimotonico@gmail.com>
2023-09-18 02:49:58 +0900

    (chore) Fix custom/rule.score.ts & custom/rule.vgoal.ts
    `npm run build`したときに発生するtscのunused varのエラーを抑制。
    良い方法が思いつかなかったので、変数名にアンダーバーつけて対応

--- client/src/custom/rule.score.ts
+++ client/src/custom/rule.score.ts
@@ -5,7 +5,8 @@ type ScoreOutputType = {
   refs?: Record<string, number>,
 } | number;
 
-export function score(stat: CurrentMatchStateType): ScoreOutputType {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function score(_stat: CurrentMatchStateType): ScoreOutputType {
   // ここにスコアの計算処理をかく
   //
   // config.json の rule.score に `"format": "implement"` を指定した場合のみ、
@@ -15,7 +16,7 @@ export function score(stat: CurrentMatchStateType): ScoreOutputType {
   // config.json の rule.score に `"format": "simple"` を指定する方が簡単
   //
   // ```
-  // return stat.taskObjects["A_1_point"] + stat.taskObjects["B_10_point"] * 10;
+  // return _stat.taskObjects["A_1_point"] + _stat.taskObjects["B_10_point"] * 10;
   // ```
 
   return NaN;
