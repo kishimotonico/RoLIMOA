6463a3cc86aa4e607f154069ce21d98ab0f7c716
kishimotonico <kishimotonico@gmail.com>
2024-02-25 08:48:46 +0900

    Update config.json: 「H.雛壇完成」とVゴール条件を追加
    内裏雛と三人官女が規定数配置されたら自動で雛壇完成が判定されるように
    ラフに実装。

--- client/src/components/ScoreInput/custom/CustomScoreInput.tsx
+++ client/src/components/ScoreInput/custom/CustomScoreInput.tsx
@@ -19,45 +19,65 @@ export const CustomScoreInput: FC<CustomScoreInputProps> = ({
   const globalObj = useSelector<RootState, Record<string, number>>((state) => state.score.global);
   const taskObj = useSelector<RootState, Record<string, number>>((state) => state.score.fields[fieldSide].tasks);
   const dispatch = useDispatch();
-  const update = (id: string) => ((value: number) => {
-    const props = {
+
+  const globalUpdate = (id: string) => ((value: number) => {
+    const action = scoreStateSlice.actions.setGloablUpdate({
       taskObjectId: id,
       afterValue: value,
-    };
-    const action = id.startsWith('shop') // XXX: 使い捨てコードなので雑に分岐
-            ? scoreStateSlice.actions.setGloablUpdate(props)
-            : scoreStateSlice.actions.setTaskUpdate({ fieldSide, ...props });
-
+    });
     LyricalSocket.dispatch(action, dispatch);
+  });
+
+  const taskUpdate = (id: string) => ((value: number) => {
+    const actions = [scoreStateSlice.actions.setTaskUpdate({
+      fieldSide,
+      taskObjectId: id,
+      afterValue: value,
+    })];
+
+    // XXX: 条件を満たしたとき「H.雛壇完成」を自動で達成する雑な実装
+    if (id === 'rule_F' || id === 'rule_G') {
+      const rule_F = id === 'rule_F' ? value : taskObj['rule_F'];
+      const rule_G = id === 'rule_G' ? value : taskObj['rule_G'];
+
+      actions.push(scoreStateSlice.actions.setTaskUpdate({
+        fieldSide,
+        taskObjectId: 'rule_H',
+        afterValue: (rule_F == 2 && rule_G == 3) ? 1 : 0,
+      }));
+    }
+
+    LyricalSocket.dispatch(actions, dispatch);
   });
 
   const color = fieldSide === 'blue' ? 'primary' : 'secondary';
 
   return <>
-    <Grid item md={3} xs={6}>
-      <Paper sx={{ p: 2, userSelect: "none" }}>
-        <Stack spacing={1} sx={{ alignItems: 'center' }}>
+    <Grid item md={3} sm={4} xs={6}>
+      <Paper sx={{ p: 2.6, userSelect: "none" }}>
+        <Stack spacing={2} sx={{ alignItems: 'center' }}>
           <Typography variant='h6'>売店エリア</Typography>
-          <CustomToggleButton label='1' value={globalObj['shop_1']} onChange={update('shop_1')} />
-          <CustomToggleButton label='2' value={globalObj['shop_2']} onChange={update('shop_2')} />
-          <CustomToggleButton label='3' value={globalObj['shop_3']} onChange={update('shop_3')} />
-          <CustomToggleButton label='4' value={globalObj['shop_4']} onChange={update('shop_4')} />
-          <CustomToggleButton label='5' value={globalObj['shop_5']} onChange={update('shop_5')} />
-          <CustomToggleButton label='6' value={globalObj['shop_6']} onChange={update('shop_6')} />
-          <CustomToggleButton label='7' value={globalObj['shop_7']} onChange={update('shop_7')} />
-          <CustomToggleButton label='8' value={globalObj['shop_8']} onChange={update('shop_8')} />
+          <CustomToggleButton label='1' value={globalObj['shop_1']} onChange={globalUpdate('shop_1')} />
+          <CustomToggleButton label='2' value={globalObj['shop_2']} onChange={globalUpdate('shop_2')} />
+          <CustomToggleButton label='3' value={globalObj['shop_3']} onChange={globalUpdate('shop_3')} />
+          <CustomToggleButton label='4' value={globalObj['shop_4']} onChange={globalUpdate('shop_4')} />
+          <CustomToggleButton label='5' value={globalObj['shop_5']} onChange={globalUpdate('shop_5')} />
+          <CustomToggleButton label='6' value={globalObj['shop_6']} onChange={globalUpdate('shop_6')} />
+          <CustomToggleButton label='7' value={globalObj['shop_7']} onChange={globalUpdate('shop_7')} />
+          <CustomToggleButton label='8' value={globalObj['shop_8']} onChange={globalUpdate('shop_8')} />
         </Stack>
       </Paper>
     </Grid>
-    <Grid item md={9} xs={6}>
+    <Grid item md={9} sm={8} xs={6}>
       <Stack spacing={1} >
-        <CustomSwitchButton label='A. 台座に接触' value={taskObj['rule_A']} onChange={update('rule_A')} color={color} />
-        <CustomPluseMinuseButton label='B. 雛人形が接地' value={taskObj['rule_B']} onChange={update('rule_B')} color={color} max={5}/>
-        <CustomSwitchButton label='C. 大台座が接地' value={taskObj['rule_C']} onChange={update('rule_C')} color={color} />
-        <CustomSwitchButton label='D. 小台座が接地' value={taskObj['rule_D']} onChange={update('rule_D')} color={color} />
-        <CustomSwitchButton label='E. 台座完成' value={taskObj['rule_E']} onChange={update('rule_E')} color={color} />
-        <CustomPluseMinuseButton label='F. 内裏雛を配置' value={taskObj['rule_F']} onChange={update('rule_F')} color={color} max={2} />
-        <CustomPluseMinuseButton label='G. 三人官女を配置' value={taskObj['rule_G']} onChange={update('rule_G')} color={color} max={3} />
+        <CustomSwitchButton label='A. 台座に接触' value={taskObj['rule_A']} onChange={taskUpdate('rule_A')} color={color} />
+        <CustomPluseMinuseButton label='B. 雛人形が接地' value={taskObj['rule_B']} onChange={taskUpdate('rule_B')} color={color} max={5}/>
+        <CustomSwitchButton label='C. 大台座が接地' value={taskObj['rule_C']} onChange={taskUpdate('rule_C')} color={color} />
+        <CustomSwitchButton label='D. 小台座が接地' value={taskObj['rule_D']} onChange={taskUpdate('rule_D')} color={color} />
+        <CustomSwitchButton label='E. 台座完成' value={taskObj['rule_E']} onChange={taskUpdate('rule_E')} color={color} />
+        <CustomPluseMinuseButton label='F. 内裏雛を配置' value={taskObj['rule_F']} onChange={taskUpdate('rule_F')} color={color} max={2} />
+        <CustomPluseMinuseButton label='G. 三人官女を配置' value={taskObj['rule_G']} onChange={taskUpdate('rule_G')} color={color} max={3} />
+        <CustomSwitchButton label='H. 雛壇完成' value={taskObj['rule_H']} onChange={taskUpdate('rule_H')} color={color} />
       </Stack>
     </Grid>
   </>;
