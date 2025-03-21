75639c4878b6d27e1d87a60d43260c1dcd5ad48b
kishimotonico <kishimotonico@gmail.com>
2025-03-21 11:06:35 +0900

    Update usePlaySoundEffect.tsxにAudio要素のプリロードを実装

--- client/src/functional/usePlaySoundEffect.tsx
+++ client/src/functional/usePlaySoundEffect.tsx
@@ -1,12 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/slices';
 import { PhaseState } from '@/slices/phase';
 import * as Phase from '@/util/PhaseStateUtil';
+import { config } from '@/config/load';
+
+const soundNames = config.time_progress.map((tp) => tp.custom?.map((cus) => cus.sound)).flat().filter((s) => s) as string[];
 
 export const usePlaySoundEffect = () => {
   const phaseState = useSelector<RootState, PhaseState>((state) => state.phase);
 
+  const audiosRef = useRef<null | { [name: string]: HTMLAudioElement }>(null);
+
+  // 音声のプリロード
+  useEffect(() => {
+    soundNames.forEach((name) => {
+      if (!audiosRef.current?.[name]) {
+        const audio = new Audio(name);
+        audio.load();
+
+        console.log(`audio preload: ${name}`);
+
+        audiosRef.current = {
+          ...audiosRef.current,
+          [name]: audio,
+        };
+      }
+    });
+  }, []);
+
+  // 効果音の再生
   useEffect(() => {
     const phaseConfig = Phase.getConfig(phaseState.current.id);
     const elapsedSec = phaseState.elapsedSecond;
@@ -16,9 +39,13 @@ export const usePlaySoundEffect = () => {
       return;
     }
 
-    // XXX: ChromeでなくFirefoxのみに対応。そのうちちゃんとライブラリ入れてやりたい
-    const audio = new Audio(matched.sound);
-    audio.play();
-
+    const audio = audiosRef.current?.[matched.sound];
+    if (audio) {
+      audio.play();
+      console.debug(`play audio ${matched.sound}`);
+    } else {
+      (new Audio(matched.sound)).play();
+      console.warn(`audio ${matched.sound} is not preloaded`);
+    }
   }, [phaseState]);
 };
