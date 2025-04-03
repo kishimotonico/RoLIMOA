59bd7514d48a6f4014517fcdd82194caeeffecfa
kishimotonico <kishimotonico@gmail.com>
2025-04-03 13:37:40 +0900

    配信画面のコンポーネントを components/StreamingOverlay 以下に分割
    今まで大会ごとに無理矢理なカスタマイズをしていたので、
    やり易いようにコンポーネントに分割

--- packages/client/src/pages/StreamingOverlayPage.tsx
+++ packages/client/src/pages/StreamingOverlayPage.tsx
@@ -1,178 +1,14 @@
-import type { FC, Ref } from 'react';
 import { useSelector } from 'react-redux';
-import type { RootState } from '@rolimoa/common/redux';
-import type { FieldSideType } from '@rolimoa/common/redux';
-import { Box, Divider, Slide } from '@mui/material';
-import { useDisplayScore } from '@/functional/useDisplayScore';
-import { useDisplayTimer } from '@/functional/useDisplayTimer';
+import { RootState } from '@/slices';
+import { Box, Slide } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
-import { CenterFlex } from '@/ui/CenterFlex';
-import { formatTime } from '@/util/formatTime';
-import { config } from '@rolimoa/common/config';
-import { SlideTransition } from '@/ui/SlideTransition';
+import { MainHud } from '@/components/StreamingOverlay/simple';
 
-type ScoreBlockProps = {
-  fieldSide: FieldSideType;
-  placement: 'left' | 'right';
-};
-
-const ScoreBlock: FC<ScoreBlockProps> = ({ fieldSide, placement }) => {
-  const teamName = useSelector<RootState, string | undefined>(
-    (state) => state.match.teams[fieldSide]?.shortName,
-  );
-  const displayScore = useDisplayScore(fieldSide);
-
-  const color = fieldSide as string; // "blue" | "red"をそのまま文字列として使う
-
-  const containerHeight = 260;
-  const outlineBorderWidth = 8;
-  const innerBorderWidth = 6;
-  const nameBlockHeight = 80;
-  const scoreBlockHeight =
-    16 +
-    containerHeight -
-    nameBlockHeight -
-    outlineBorderWidth * 2 -
-    innerBorderWidth; // 文字を下に下げるため微調整
-
-  let teamNameFontSize = 40;
-  if (teamName && teamName?.length > 12) {
-    teamNameFontSize = 36;
-  }
-  if (teamName && teamName?.length > 14) {
-    teamNameFontSize = 30;
-  }
-
-  return (
-    <Box>
-      <Box
-        sx={{
-          width: '600px',
-          height: `${containerHeight}px`,
-          textAlign: 'center',
-          border: `${outlineBorderWidth}px solid ${color}`,
-          boxSizing: 'border-box',
-          backgroundColor: 'rgba(240, 240, 240, 0.8)',
-        }}
-      >
-        <CenterFlex
-          sx={{
-            height: `${nameBlockHeight}px`,
-            lineHeight: `${nameBlockHeight}px`,
-            borderBottom: `${innerBorderWidth}px solid ${color}`,
-            fontSize: `${teamNameFontSize}px`,
-          }}
-        >
-          {teamName ?? ' '}
-        </CenterFlex>
-        <CenterFlex
-          sx={{
-            height: `${scoreBlockHeight}px`,
-            fontSize: '100px',
-            flexDirection: placement === 'left' ? 'row' : 'row-reverse',
-          }}
-        >
-          {displayScore.scoreState.vgoal && (
-            <Box sx={{ fontSize: '40px' }}>
-              <Box>{config.rule.vgoal.name}</Box>
-              <Box>🏴 {formatTime(displayScore.scoreState.vgoal, 'm:ss')}</Box>
-            </Box>
-          )}
-          <Box sx={{ padding: '0 .5em', lineHeight: `${scoreBlockHeight}px` }}>
-            {displayScore.value}
-          </Box>
-        </CenterFlex>
-      </Box>
-      <Box
-        sx={{
-          display: 'flex',
-          width: '100%',
-          justifyContent: placement === 'left' ? 'flex-start' : 'flex-end',
-        }}
-      >
-        {displayScore.scoreState.winner && (
-          <CenterFlex
-            sx={{
-              width: '240px',
-              height: '60px',
-              backgroundColor: `${color}`,
-              fontSize: '42px',
-              color: 'rgba(255, 255, 255, 0.95)',
-            }}
-          >
-            Winner
-          </CenterFlex>
-        )}
-      </Box>
-    </Box>
-  );
-};
-
-const TimerDisplay: FC<{ ref?: Ref<null> }> = ({ ref }) => {
-  const { description, displayTime } = useDisplayTimer();
-  const matchName = useSelector<RootState, string>((state) => state.match.name);
-
-  return (
-    <>
-      <Box
-        ref={ref}
-        sx={{
-          width: '400px',
-          height: '260px',
-          textAlign: 'center',
-          backgroundColor: 'rgba(10, 10, 10, 0.9)',
-          boxSizing: 'border-box',
-          color: 'rgba(240, 240, 240, 0.95)',
-          zIndex: 10,
-        }}
-      >
-        <Box
-          sx={{
-            height: '70px',
-            lineHeight: '70px',
-            fontSize: '24px',
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          {description}
-        </Box>
-        <Box
-          sx={{
-            fontFamily: 'DSEG14-Classic',
-            fontWeight: 500,
-            height: '120px',
-            lineHeight: '120px',
-            fontSize: '72px',
-          }}
-        >
-          {displayTime}
-        </Box>
-        <Divider
-          sx={{
-            margin: '0 30px',
-            borderColor: 'rgba(240, 240, 240, 0.5)',
-          }}
-        />
-        <Box
-          sx={{
-            height: '69px',
-            lineHeight: '69px',
-            fontSize: '24px',
-          }}
-        >
-          {matchName ?? ''}
-        </Box>
-      </Box>
-    </>
-  );
-};
-
-export type StreamingOverlayPageParams = {
+type StreamingOverlayPageParams = {
   reverse: boolean;
 };
 
-export const StreamingOverlayPage: FC = () => {
+export const StreamingOverlayPage = () => {
   const [searchParams] = useSearchParams();
   const params: StreamingOverlayPageParams = {
     reverse: searchParams.get('reverse') !== null,
@@ -184,6 +20,7 @@ export const StreamingOverlayPage: FC = () => {
   const showScoreBoard = useSelector<RootState, boolean>(
     (state) => state.streamingInterface.showScoreBoard,
   );
+  const showSubHud = false;
 
   return (
     <Box
@@ -197,42 +34,20 @@ export const StreamingOverlayPage: FC = () => {
       }}
     >
       <Slide in={showMainHud} direction="down" timeout={1000} appear={false}>
-        <Box
-          sx={{
-            width: '100%',
-            height: '320px',
-            display: 'flex',
-          }}
-        >
-          <SlideTransition
-            in={showScoreBoard}
-            direction="left"
-            duration={800}
-            appear={false}
-          >
-            <Box>
-              <ScoreBlock
-                fieldSide={params.reverse ? 'blue' : 'red'}
-                placement="left"
-              />
-            </Box>
-          </SlideTransition>
-          <TimerDisplay />
-          <SlideTransition
-            in={showScoreBoard}
-            direction="right"
-            duration={800}
-            appear={false}
-          >
-            <Box>
-              <ScoreBlock
-                fieldSide={params.reverse ? 'red' : 'blue'}
-                placement="right"
-              />
-            </Box>
-          </SlideTransition>
-        </Box>
+        <MainHud showScoreBoard={showScoreBoard} params={params} />
       </Slide>
+      <Box
+        sx={{
+          width: '100%',
+          position: 'absolute',
+          display: 'flex',
+          justifyContent: 'flex-end',
+        }}
+      >
+        <Slide in={showSubHud} direction="left" timeout={800} appear={false}>
+          <Box sx={{ width: '160px' }}>{/* <SubHudDisplay /> */}</Box>
+        </Slide>
+      </Box>
     </Box>
   );
 };
